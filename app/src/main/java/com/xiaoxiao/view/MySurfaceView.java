b0b783aba1e0d1d160ea9fb2ec3fa01854517f17
hanwj <hanwj>
2016-08-04 13:16:31 +0800

    fixbug

--- app/src/main/java/com/xiaoxiao/view/MySurfaceView.java
+++ app/src/main/java/com/xiaoxiao/view/MySurfaceView.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
